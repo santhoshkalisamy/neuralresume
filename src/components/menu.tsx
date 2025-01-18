fb137921aab9ad6de8b31808b35b250d799b4317
Santhosh K <santhoshkalisamy@gmail.com>
2025-01-18 13:35:28 +0530

    Logout fix

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -121,7 +121,7 @@ export function Menu({ isOpen }: MenuProps) {
               <Tooltip delayDuration={100}>
                 <TooltipTrigger asChild>
                   <Button
-                    onClick={() => {signOut()}}
+                    onClick={() => {signOut().then(() => window.location.reload())}}
                     variant="outline"
                     className="w-full justify-center h-10 mt-5"
                   >
